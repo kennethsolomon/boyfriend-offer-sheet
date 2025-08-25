7a41a2c66d6b87360dd9c29a60c96fc7d6d85039
Kenneth Solomon <hello@kennethsolomon.com>
2025-08-25 12:39:39 +0800

    Enhance Vite configuration to allow external connections by specifying allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['boyfriend.kennethsolomon.com', 'localhost', '127.0.0.1'],
+    host: true // Allow external connections
+  }
 })
